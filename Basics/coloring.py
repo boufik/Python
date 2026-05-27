c8d6e315246575bec6e7ca63a711ed28286e36ee
Thomas Boufikos <106670035+boufik@users.noreply.github.com>
2026-05-27 17:53:35 +0300

    Create coloring.py

---
+++ Basics/coloring.py
@@ -0,0 +1,12 @@
+# ANSI Colors
+RESET  = "\033[0m"
+GREEN  = "\033[92m"
+RED    = "\033[91m"
+YELLOW = "\033[93m"
+CYAN   = "\033[96m"
+
+# Prints
+print(f"{GREEN}GREEN{RESET}")
+print(f"{RED}RED{RESET}")
+print(f"{YELLOW}YELLOW{RESET}")
+print(f"{CYAN}CYAN{RESET}")
